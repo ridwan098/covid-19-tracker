39a8fc94887e9add78f641a862cb3405875d4008
Ridwan <ridwanishola97@gmail.com>
2020-06-27 02:58:41 +0100

    card is working

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css'
+
+
+
+const Cards = (props) => {
+    console.log(props)
+
+
+    return (
+        <div className={StyleSheet.container}>
+            <Grid container spacing={3} justify='center'>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Infected</Typography>
+                        <Typography variant='h5'>Real Data</Typography>
+                        <Typography color='textSecondary'>Real Date</Typography>
+                        <Typography variant='body2'>Number of active cases of COVID-1   9</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default Cards;
